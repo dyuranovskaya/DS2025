51b31e6c786eba09a076715e082cba2b25e71f60
ruslanbekovaaidana8 <ruslanbekovaaidana8@mail.com>
2025-03-24 14:28:44 +0900

    v0.5 array

--- week03.py
+++ week03.py
@@ -1,12 +1,4 @@
-def move_zeros(a_list):
-    zero_index = 0
-    for index, n in enumerate(a_list):
-        if n != 0 :
-            a_list[zero_index] = n
-            if zero_index != index:
-                a_list[index] = 0
-                zero_index += 1
-    return (a_list)
-a_list = [99,0,-7,0,16]
-move_zeros(a_list)
-print(a_list)
+import array
+arr = array.array('f', [99, 0, -7, 0, 16])
+for i in range(len(arr)):
+    print(f"{arr[i]:5} {id(arr[i])}")
