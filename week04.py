81a46e9ce83657becb3cd66fa8ade224ab268e15
ruslanbekovaaidana8 <ruslanbekovaaidana8@mail.com>
2025-03-31 14:36:58 +0900

    1.3linked list

--- week04.py
+++ week04.py
@@ -1,23 +1,34 @@
 class Node:
-    def __init__(self,data, link = None):
+    def __init__(self, data, link=None):
         self.data = data
         self.link = link
 
 class Linkedlist:
     def __init__(self):
         self.head = None
-    def append(self,data):
+
+    def append(self, data):
         if not self.head:
             self.head = Node(data)
             return
         current = self.head
         while current.link:
             current = current.link
-            current.link = Node(data)
+        current.link = Node(data)
+
+    def __str__(self):
+        current = self.head
+        out_texts = ""
+        while current is not None:
+            # out_texts = out_texts + str(current.data) + "->"
+            out_texts = out_texts + f"{current.data}->"
+            current = current.link
+        return out_texts + "END"
+
 
 ll = Linkedlist()
 ll.append(8)
+ll.append(9)
 ll.append(10)
-ll.append(-9)
 
 print(ll)
