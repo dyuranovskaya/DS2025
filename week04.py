ec4ff02c8636ffddf4abad77e43b6c0e02faab01
ruslanbekovaaidana8 <ruslanbekovaaidana8@mail.com>
2025-03-31 15:45:23 +0900

    v1.8 linked list Q&A

--- week04.py
+++ week04.py
@@ -18,14 +18,17 @@ class LinkedList:
         current.link = Node(data)
 
     def remove(self, target):
-        if self.head.data == target:
-            self.head = self.head.link
-            return
         current = self.head
+        # if self.head.data == target:
+        if current.data == target:
+            self.head = self.head.link
+            current.link = None
+            return
         previous = None
         while current:
             if current.data == target:
                 previous.link = current.link
+                current.link = None
             previous = current
             current = current.link
 
