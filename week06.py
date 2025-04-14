62571fc56bf0a9c67c2a892546d749bfda5e5c34
ruslanbekovaaidana8 <ruslanbekovaaidana8@mail.com>
2025-04-14 14:12:48 +0900

    v2.1queue

---
+++ week06.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self,data,link = None):
+        self.data = data
+        self.link = link
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+        self.size = 0
+    def enqueue (self,data): #вторая очередь за ним()
+        #q.enqueue("DataBase")
+        #q.enqueue("Data Structure")
+        self.size = self.size + 1
+        node = Node(data)
+        if self.rear is None:
+            self.front = node
+            self.rear = node
+
+        else:
+            self.rear.link = node
+            self.rear = node
+q = Queue()
+q.enqueue("DataBase")
+q.enqueue("Data Structure")
+print(q.size,q.front.data,q.rear.data)
