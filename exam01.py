4c7b58ee17aae914749bbcdd2ec3ebf870322491
ruslanbekovaaidana8 <ruslanbekovaaidana8@mail.com>
2025-04-20 20:55:01 +0900

    linked.list

--- exam01.py
+++ exam01.py
@@ -1,22 +1,14 @@
-# numbers = [5,8,13]
-# print(numbers[1])
-# numbers.append(21)
-# print(numbers)
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+node1 = Node(50)
+node2 = Node(100)
+node3 = Node(200)
 
-# numbers = [7,14,21]
-# print(numbers[0])
-# numbers.append(28)
-# print(numbers)
-
-# nums = [3,6,9,12]
-# print(nums[2])
-# nums.pop()
-# print(nums)
-numbers = [8,16,32,64]
-print(numbers[3])
-numbers.append(128)
-numbers.pop()
-print(numbers)
-
-
-
+node1.next = node2
+node2.next = node3
+current = node1
+while current:
+    print(current.data)
+    current = current.next
