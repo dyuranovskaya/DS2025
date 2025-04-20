4d2c72608ab7b4ed7fe1795ad14b78f73de9c927
ruslanbekovaaidana8 <ruslanbekovaaidana8@mail.com>
2025-04-20 21:58:04 +0900

    stack

--- exam01.py
+++ exam01.py
@@ -1,14 +1,46 @@
-class Node:
-    def __init__(self,data):
-        self.data = data
-        self.next = None
-node1 = Node(50)
-node2 = Node(100)
-node3 = Node(200)
+# stack = []
+# stack.append(5)
+# stack.append(10)
+# stack.append(15)
+# print(stack.pop(-1))
+# print(stack)
 
-node1.next = node2
-node2.next = node3
-current = node1
-while current:
-    print(current.data)
-    current = current.next
+# stack = []
+# stack.append(100)
+# stack.append(200)
+# stack.append(300)
+# stack.append(400)
+# print(stack.pop())
+# print(stack.pop(-1))
+# print(stack)
+# stack = []
+# stack.append(1)
+# stack.append(2)
+# stack.append(3)
+# stack.append(4)
+# stack.append(5)
+# top1 = stack.pop()
+# top2 = stack.pop()
+# print(f'top1:{top1}\ntop2:{top2}')
+# print(stack)
+
+
+# stack = []
+# stack.append(99)
+# top1 = stack.pop()
+# if not stack:
+#     print("더 이상 꺼낼 데이터가 없습니다")
+# print(stack)
+stack = []
+stack.append(10)
+stack.append(20)
+stack.append([30,40,50])
+stack.append(60)
+print(f'a:{stack.pop()}')
+print(f'b:{stack.pop()}')
+print(f'stack:{stack}')
+
+
+
+
+
