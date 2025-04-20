82323533b7727011a39e4db8fdcfa885de3cb78d
ruslanbekovaaidana8 <ruslanbekovaaidana8@mail.com>
2025-04-20 22:38:29 +0900

    queue

--- exam01.py
+++ exam01.py
@@ -1,46 +1,14 @@
-# stack = []
-# stack.append(5)
-# stack.append(10)
-# stack.append(15)
-# print(stack.pop(-1))
-# print(stack)
-
-# stack = []
-# stack.append(100)
-# stack.append(200)
-# stack.append(300)
-# stack.append(400)
-# print(stack.pop())
-# print(stack.pop(-1))
-# print(stack)
-# stack = []
-# stack.append(1)
-# stack.append(2)
-# stack.append(3)
-# stack.append(4)
-# stack.append(5)
-# top1 = stack.pop()
-# top2 = stack.pop()
-# print(f'top1:{top1}\ntop2:{top2}')
-# print(stack)
-
-
-# stack = []
-# stack.append(99)
-# top1 = stack.pop()
-# if not stack:
-#     print("더 이상 꺼낼 데이터가 없습니다")
-# print(stack)
-stack = []
-stack.append(10)
-stack.append(20)
-stack.append([30,40,50])
-stack.append(60)
-print(f'a:{stack.pop()}')
-print(f'b:{stack.pop()}')
-print(f'stack:{stack}')
-
-
-
-
-
+from collections import deque
+queue = deque()
+queue.append(1)
+queue.append(2)
+queue.append(3)
+queue.append(4)
+queue.append(5)
+first_out = queue.popleft()
+print(first_out)
+queue.append(6)
+queue.append(7)
+second_out = queue.popleft()
+print(second_out)
+print(queue)
