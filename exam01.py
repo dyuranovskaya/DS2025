d19d070212c67e1d4983ee74657028bdc31a4926
ruslanbekovaaidana8 <ruslanbekovaaidana8@mail.com>
2025-04-20 20:34:18 +0900

    arrayEX

---
+++ exam01.py
@@ -0,0 +1,22 @@
+# numbers = [5,8,13]
+# print(numbers[1])
+# numbers.append(21)
+# print(numbers)
+
+# numbers = [7,14,21]
+# print(numbers[0])
+# numbers.append(28)
+# print(numbers)
+
+# nums = [3,6,9,12]
+# print(nums[2])
+# nums.pop()
+# print(nums)
+numbers = [8,16,32,64]
+print(numbers[3])
+numbers.append(128)
+numbers.pop()
+print(numbers)
+
+
+
