71f8a894cb25fe0308f94cd697471ef6274305a5
ruslanbekovaaidana8 <ruslanbekovaaidana8@mail.com>
2025-04-28 15:41:24 +0900

    v3.5tree

--- week08.py
+++ week08.py
@@ -10,28 +10,51 @@ def post_order(node):
         post_order(node.left)
         post_order(node.right)
 
+def insert(root,value):
+    new_node = TreeNode()
+    new_node.data = value
+    if root is None:
+        return new_node
+
+    while True:
+        if value < current.data:
+            if current.left is None:
+                current.left = new_node
+                break
+            current = current.left  # move
+        else:
+            if current.right is None:
+                current.right = new_node
+                break
+            current = current.right
+    return root
+
+
+
+
 
 if __name__ == "__main__":
     numbers = [10,15,8,3,9]
     root = None
-    node = TreeNode()
-    node.data = numbers[0]
-    root = node
+
     for number in numbers[1:]:
-        node = TreeNode()
-        node.data = data = number
-        current = root
-        while True:
-            if number < current.data:
-                if current.left is None:
-                    current.left = node
-                    break
-                current = current.left #move
-            else:
-                if current.right is None:
-                    current.right = node
-                    break
-                current = current.right
+        root = insert(root,number)
 
 print("BST 구성 완료")
 post_order(root)
+# find_number = int(input())
+#
+# while True:
+#     if find_number == current.data:
+#         print(f"{find_number}를(을) 찾았습니다.")
+#         break
+#     elif find_number < current.data:
+#         if current.left is None:
+#             print(f"{find_number}이(가) 존재하지 않습니다.")
+#             break
+#         current = current.left
+#     else:
+#         if current.right is None:
+#             print(f"{find_number}이(가) 존재하지 않습니다.")
+#             break
+#         current = current.right
