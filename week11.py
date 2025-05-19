17f620a404e371122d9253c4c958264d1e98c545
ruslanbekovaaidana8 <ruslanbekovaaidana8@mail.com>
2025-05-19 14:17:32 +0900

    4.5graph

--- week11.py
+++ week11.py
@@ -1,14 +1,18 @@
 graph = [
     [0, 1, 1, 0, 0, 0, 0, 0],
-    [1, 0, 1, 0, 0, 0, 0, 0],
-    [1, 1, 0, 1, 0, 0, 0, 0],
-    [0, 0, 1, 0, 1, 1, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 0, 1, 1, 1, 0],
     [0, 0, 0, 1, 0, 1, 0, 0],
-    [0, 0, 0, 1, 1, 0, 1, 1],
-    [0, 0, 0, 0, 0, 1, 0, 1],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 1],
     [0, 0, 0, 0, 0, 0, 1, 0]
 ]
 def dfs(g,i,visited):
-    pass
+    visited[i] = 1
+    print(chr(ord('A')+i),end='')
+    for j in range(len(graph)):
+        if g[i][j] == 1 and not visited[j]:
+            dfs(g,j,visited)
 visited_dfs = [0 for _ in range(len(graph))]
 dfs(graph,0,visited_dfs)
